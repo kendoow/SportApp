dea7340b7d4eae4c70cbc2759746d9e6100b7c31
kendoow <kendo_ow@mail.ru>
2023-12-09 18:48:55 +0300

    removed dropdown

--- frontend/main/src/entities/NavigationSidebar/ui/NavigationSidebar.tsx
+++ frontend/main/src/entities/NavigationSidebar/ui/NavigationSidebar.tsx
@@ -1,39 +1,24 @@
 import { NavLink } from "react-router-dom";
 import cn from "classnames";
-
-import Dropdown from "@shared/ui/DropDown/DropDown";
 import { mainPagesOptions } from "@entities/NavigationSidebar/constans";
 import styles from "./NavigationSidebar.module.scss";
 
 const NavigationSidebar = () => {
   return (
     <div className={styles.sidebar}>
-      <Dropdown title="main pages">
-        {mainPagesOptions.map((option) => (
-          <NavLink
-            to={`/${option}`}
-            className={({ isActive }) =>
-              isActive
-                ? cn(styles.sidebarActive, styles.sidebarElement)
-                : cn(styles.sidebarElement, styles.sidebarInactive)
-            }
-          >
-            {option}
-          </NavLink>
-        ))}
-      </Dropdown>
-      <Dropdown title="settings">
+      <h3 title="main pages" />
+      {mainPagesOptions.map((option) => (
         <NavLink
-          to="/account"
+          to={`/${option}`}
           className={({ isActive }) =>
             isActive
               ? cn(styles.sidebarActive, styles.sidebarElement)
               : cn(styles.sidebarElement, styles.sidebarInactive)
           }
         >
-          account
+          {option}
         </NavLink>
-      </Dropdown>
+      ))}
     </div>
   );
 };
