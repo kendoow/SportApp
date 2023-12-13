cbbefba616444647b9ddf1bee8a2462b941ad32a
kendoow <kendo_ow@mail.ru>
2023-12-14 01:28:32 +0300

    swagger added

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	_ "github.com/kendoow/SportApp/backend/docs"
 	"github.com/kendoow/SportApp/backend/internal/routes"
 	"github.com/kendoow/SportApp/backend/internal/utils"
 	"github.com/kendoow/SportApp/backend/middleware"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"net/http"
 	"os"
 )
+
+// @title           SportApp API
+// @version         1.0
+// @description     API Server for SportApp application.
+
+// @host      localhost:8080
+// @BasePath  /
+
+// @securityDefinitions.apikey  ApiKeyAuth
+// @in header
+// @name Authorization
 
 func main() {
 	r := mux.NewRouter()
 
 	routes.AuthRoutes(r)
 	routes.HealthRoutes(r)
-
+	r.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 	handler := middleware.Logging(r)
 	handler = middleware.PanicCatching(handler)
 	handler = middleware.CorsMiddleware(handler)
