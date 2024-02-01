5866111faa5640d81767fb9670893ea155c4fc3e
kendoow <kendo_ow@mail.ru>
2024-02-02 00:36:58 +0300

    [FRONTEND] Home page banner

--- frontend/main/src/entities/Home/ui/HomeBanner/HomeBanner.tsx
+++ frontend/main/src/entities/Home/ui/HomeBanner/HomeBanner.tsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import { FC } from 'react';
 
 import { HomeBannerProps } from '@entities/Home/model/types';
+import ArrowIcon from '@shared/ui/Icons/ArrowIcon';
 import WreathIcon from '@shared/ui/Icons/WreathIcon';
 
 import styles from './HomeBanner.module.scss';
@@ -26,7 +28,32 @@ const HomeBanner: FC<HomeBannerProps> = ({ name, totalDays, streak, betterPercen
           <h3 className={styles.info}>{betterPercent}% of users</h3>
         </div>
       </div>
-      <div className={styles.userStats}>1</div>
+
+      <div className={styles.userStats}>
+        <div className={styles.userStats}>
+          <div className={styles.baseStats}>
+            <p className={styles.baseStatsName}>height</p>
+            <h3 className={styles.baseStatsValue}>{height} sm</h3>
+          </div>
+          <div className={styles.baseStats}>
+            <p className={styles.baseStatsName}>weight</p>
+            <h3 className={styles.baseStatsValue}>{weight} kg</h3>
+          </div>
+          <div className={styles.baseStats}>
+            <p className={styles.baseStatsName}>body fat</p>
+            <h3 className={styles.baseStatsValue}>{bodyFat}% kg</h3>
+          </div>
+        </div>
+        <div className={styles.linkBlock}>
+          <Link
+            className={styles.link}
+            to={'/bio'}
+          >
+            go to bio
+          </Link>
+          <ArrowIcon className={styles.icon} />
+        </div>
+      </div>
     </div>
   );
 };
