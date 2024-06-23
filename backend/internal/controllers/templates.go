05c394674208b5ba6931eebfd862f095b393d890
kendoow <kendo_ow@mail.ru>
2024-06-23 15:58:50 +0300

    templates CRUD

--- backend/internal/controllers/templates.go
+++ backend/internal/controllers/templates.go
@@ -1,7 +1,78 @@
 package controllers
 
-import "net/http"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/kendoow/SportApp/backend/internal/model"
+	"github.com/kendoow/SportApp/backend/internal/utils"
+	"net/http"
+)
 
-func GetAllTemplates(w http.ResponseWriter, r *http.Request) {
+func (controller *Controller) GetAllTemplates(w http.ResponseWriter, r *http.Request) {
+	service := controller.service
+	templates, err := service.GetAllTemplates()
+	if err != nil {
+		utils.Error.Println(err.Error())
+		http.Error(w, "Request ends with err", http.StatusInternalServerError)
+		return
+	}
 
+	if err := json.NewEncoder(w).Encode(templates); err != nil {
+		utils.Error.Println(err.Error())
+		http.Error(w, "Request ends with err", http.StatusInternalServerError)
+	}
+}
+
+func (controller *Controller) CreateTemplate(w http.ResponseWriter, r *http.Request) {
+	service := controller.service
+	parsedTemplate := &model.Template{}
+	fmt.Print(parsedTemplate)
+	utils.ParseBody(r, parsedTemplate)
+	template, err := service.CreateTemplate(parsedTemplate)
+	fmt.Print(template)
+	if err != nil {
+		utils.Error.Println(err.Error())
+		http.Error(w, "Creation ends with error", http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(template); err != nil {
+		utils.Error.Println(err.Error())
+		http.Error(w, "Creation ends with error", http.StatusInternalServerError)
+	}
+}
+
+func (controller *Controller) GetTemplateById(w http.ResponseWriter, r *http.Request) {
+	service := controller.service
+	vars := mux.Vars(r)
+	templateId := vars["id"]
+	utils.Info.Println(templateId)
+	template, err := service.GetTemplateById(context.TODO(), templateId)
+
+	if err != nil {
+		utils.Error.Println(err.Error())
+		http.Error(w, "Get ends with error", http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(template); err != nil {
+		utils.Error.Println(err.Error())
+		http.Error(w, "Get ends with error", http.StatusInternalServerError)
+	}
+}
+
+func (controller *Controller) DeleteTemplateById(w http.ResponseWriter, r *http.Request) {
+	service := controller.service
+	vars := mux.Vars(r)
+	templateId := vars["id"]
+	utils.Info.Println(templateId)
+	err := service.DeleteTemplateById(context.TODO(), templateId)
+
+	if err != nil {
+		utils.Error.Println(err.Error())
+		http.Error(w, "Get ends with error", http.StatusInternalServerError)
+		return
+	}
 }
--- backend/internal/services/templates.go
+++ backend/internal/services/templates.go
@@ -1,1 +1,91 @@
 package services
+
+import (
+	"context"
+	"github.com/kendoow/SportApp/backend/internal/model"
+	"github.com/kendoow/SportApp/backend/internal/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (service *Service) GetAllTemplates( /*params*/ ) (*[]*model.Template, error) {
+	repo := service.repo
+
+	cursor, err := repo.GetAllTemplates(context.TODO(), &bson.M{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(context.TODO())
+	var templates []*model.Template
+
+	for cursor.Next(context.TODO()) {
+		var elem model.Template
+
+		err := cursor.Decode(&elem)
+		if err != nil {
+			utils.Error.Println(err.Error())
+		}
+
+		templates = append(templates, &elem)
+	}
+
+	if err := cursor.Err(); err != nil {
+		utils.Error.Println(err.Error())
+		return nil, err
+	}
+
+	return &templates, nil
+}
+
+func (service *Service) CreateTemplate(createdTemplate *model.Template) (interface{}, error) {
+	repo := service.repo
+	newTemplate, err := repo.CreateTemplate(context.TODO(), createdTemplate)
+	if err != nil {
+		return "", err // TODO: rewrite empty string
+	}
+	return newTemplate.InsertedID, nil
+}
+
+func (service *Service) GetTemplateById(ctx context.Context, Id string) (*model.Template, error) {
+	repo := service.repo
+	id, err := primitive.ObjectIDFromHex(Id)
+
+	if err != nil {
+		utils.Error.Println(err)
+		return nil, err
+	}
+
+	filter := bson.D{{"_id", id}}
+	result := repo.GetTemplateById(ctx, filter)
+	var elem model.Template
+
+	err = result.Decode(&elem)
+	if err != nil {
+		utils.Error.Println(err.Error())
+	}
+	return &elem, nil
+}
+
+func (service *Service) DeleteTemplateById(ctx context.Context, Id string) error {
+	repo := service.repo
+
+	id, err := primitive.ObjectIDFromHex(Id)
+
+	if err != nil {
+		utils.Error.Println(err)
+		return err
+	}
+
+	filter := bson.D{{"_id", id}}
+	result, err := repo.DeleteTemplateById(ctx, filter)
+
+	if err != nil {
+		utils.Error.Println(err.Error())
+	}
+
+	utils.Info.Println(result)
+
+	return nil
+}
